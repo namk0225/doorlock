2ba3381ca0f9b9635cc53f7ee4cd290df322d31c
namk0225 <69349621+namk0225@users.noreply.github.com>
2020-08-07 20:30:04 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    A버튼 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    game.resume()
+})
+input.onButtonPressed(Button.B, function () {
+    B버튼 += 1
+})
+let A버튼 = 0
+let B버튼 = 0
+game.resume()
+basic.forever(function () {
+	
+})
